bc12ee90dbadfd029f111b955978ae6f9a25806d
PhatNguyen203 <Lee0ne203@gmail.com>
2019-10-18 16:28:07 -0500

    add new customer

---
+++ classes/Manager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StoreApp.Data
+{
+    public class Manager
+    {
+        public int ManagerId { get; set; }
+        public int StoreId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+}
---
+++ classes/Orders.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StoreApp.Data
+{
+    public class Orders
+    {
+        public int OrderId { get; set; }
+        public int CustomerId { get; set; }
+        public int StoreId { get; set; }
+
+        public int Ariel { get; set; }
+        public int Downie { get; set; }
+        public int Suavitel { get; set; }
+
+        public  Customer Customer { get; set; }
+        public Store Store { get; set; }
+    }
+}
