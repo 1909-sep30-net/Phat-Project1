6d65f7ceb4d5d64baca925c1a6068fa2183a3e11
PhatNguyen203 <Lee0ne203@gmail.com>
2019-10-25 10:34:23 -0500

    recover classes

---
+++ StoreApp.Data/Entities/Orders.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StoreApp.Data.Entities
+{
+    public partial class Orders
+    {
+        
+        public int OrderId { get; set; }
+        public int CustomerId { get; set; }
+        public int StoreId { get; set; }
+        public int Ariel { get; set; }
+        public int Downie { get; set; }
+        public int Suavitel { get; set; }
+
+        public virtual Customer Customer { get; set; }
+        public virtual Store Store { get; set; }
+    }
+}
